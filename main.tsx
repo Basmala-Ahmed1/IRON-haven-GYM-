a628da251d2256cfe01a45bc33f668a4a8da40f6
Basmala-Ahmed1 <159727183+Basmala-Ahmed1@users.noreply.github.com>
2026-04-03 03:58:37 +0200

    Create main.tsx file for the IRON haven GYM project

---
+++ main.tsx
@@ -0,0 +1,12 @@
+// Main component for the IRON haven GYM
+import React from 'react';
+
+const Main: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the IRON haven GYM</h1>
+        </div>
+    );
+};
+
+export default Main;
